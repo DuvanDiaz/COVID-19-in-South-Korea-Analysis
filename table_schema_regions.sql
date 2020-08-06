6c9d116cd62c06fb8021c7d0259fbc87ac7403b5
Eline Van Eldere <elinevaneldere@gmail.com>
2020-08-06 16:25:52 -0400

    adding Jupyter notebook to branch

---
+++ table_schema_regions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE regions(
+	"city" VARCHAR PRIMARY KEY NOT NULL,
+	"group"  BOOLEAN NOT NULL,
+	"infection_case" VARCHAR NOT NULL,
+	"confirmed" INTEGER NOT NULL,
+	"code" FLOAT NOT NULL
+);
+
