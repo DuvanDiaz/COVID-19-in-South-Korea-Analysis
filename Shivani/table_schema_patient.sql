424397227254e6519d028015bdd04cec60536333
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-08-06 15:55:01 -0400

    jupyter notebooks upload

---
+++ Shivani/table_schema_patient.sql
@@ -0,0 +1,10 @@
+CREATE TABLE patient_info(
+"patient_id" INTEGER PRIMARY KEY NOT NULL, 
+"sex" CHAR NOT NULL, 
+"age" VARCHAR NOT NULL, 
+"province" VARCHAR NOT NULL, 
+"city" VARCHAR NOT NULL, 
+"confirmed_date" DATE NOT NULL,
+"released_date" DATE NOT NULL, 
+"state" VARCHAR NOT NULL
+);
