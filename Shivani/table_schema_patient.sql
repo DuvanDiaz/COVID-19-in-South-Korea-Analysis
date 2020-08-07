67a08b9134ac0b905dfff1d9c3ce66e64e81f564
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-08-06 20:07:41 -0400

    final Main_etl

--- Shivani/table_schema_patient.sql
+++ Shivani/table_schema_patient.sql
@@ -1,10 +1,10 @@
 CREATE TABLE patient_info(
-"patient_id" INTEGER PRIMARY KEY NOT NULL, 
-"sex" CHAR NOT NULL, 
-"age" VARCHAR NOT NULL, 
-"province" VARCHAR NOT NULL, 
-"city" VARCHAR NOT NULL, 
-"confirmed_date" DATE NOT NULL,
-"released_date" DATE NOT NULL, 
-"state" VARCHAR NOT NULL
+"patient_id" BIGINT PRIMARY KEY NOT NULL, 
+"sex" CHAR(6) NOT NULL, 
+"age" VARCHAR(4) NOT NULL, 
+"province" VARCHAR(20) NOT NULL, 
+"city" VARCHAR(20) NOT NULL, 
+"confirmed_date" VARCHAR(10) NOT NULL,
+"released_date" VARCHAR(10) NOT NULL, 
+"state" VARCHAR(11) NOT NULL
 );
